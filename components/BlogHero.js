e6a924bc9dc129ea67e0ac02459790e9bf2c2693
boVIenChamMamTom <84711587+giabaocorn20@users.noreply.github.com>
2022-10-16 00:01:47 -0400

    test if it ok

--- components/BlogHero.js
+++ components/BlogHero.js
@@ -5,10 +5,9 @@ export default function BlogHero() {
     <Container className="py-5 text-center">
         <Row className="">
             <Col>
-                <h1 className="fw-light">My Blog</h1>
-                <p className="lead text-muted">Hi, welcome to the blog section. I and will try to blog as many entries as possible. </p>
+                <h1 className="fw-light">My Project</h1>
+                <p className="lead text-muted">Hi, welcome to the project section. I will put most of my engineering, web or even j4f projects on here! Check it out! </p>
                 <p>
-
                     <a></a>
                     <a></a>
                 </p>
