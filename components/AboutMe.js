72dff2769b142e09afd8d6e27adf14b894d834b8
boVIenChamMamTom <84711587+giabaocorn20@users.noreply.github.com>
2023-03-21 19:44:31 -0400

    fix contact me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -67,7 +67,7 @@ export default function AboutMe() {
             >
               <Flex flexDir={["column", "row", "row"]}>
                 <Link
-                  href="giabao.corn20@gmail.com"
+                  href="mailto:max.mustermann@example.com?body=Hey handsome"
                   _hover={{ textDecor: "none" }}
                   w={["100%", "100%", null]}
                   mr={[0, 2, 2]}
